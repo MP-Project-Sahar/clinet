ab39cc1f720bd05abcd56dd9088043a4f523badd
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-29 14:09:43 +0300

    not found

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -14,22 +14,27 @@ import Instruction from "./components/Instruction";
 import NotFound from "./components/NotFound";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <>
-      <NavBar />
+      {pathname !== "/404" && <NavBar />}
+
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/LoginOrSignup" element={<LoginOrSignup />} />
-        <Route exact path="/Dashboard" element={<Dashboard />} />
-        <Route exact path="/Inbox" element={<Inbox />} />
-        <Route exact path="/Rentals" element={<Rentals />} />
-        <Route exact path="/Favorites" element={<Favorites />} />
-        <Route exact path="/Profile" element={<Profile />} />
-        <Route exact path="/ListItem" element={<ListItem />} />
-        <Route exact path="/Instruction" element={<Instruction />} />
-        <Route exact path="*" element={<NotFound />} />
+        <Route exact path="LoginOrSignup" element={<LoginOrSignup />} />
+        <Route exact path="Dashboard" element={<Dashboard />} />
+        <Route exact path="Inbox" element={<Inbox />} />
+        <Route exact path="Rentals" element={<Rentals />} />
+        <Route exact path="Favorites" element={<Favorites />} />
+        <Route exact path="Profile" element={<Profile />} />
+        <Route exact path="ListItem" element={<ListItem />} />
+        <Route exact path="Instruction" element={<Instruction />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route exact path="/404" element={<NotFound />} />
       </Routes>
-      <Footer />
+      
+      {pathname !== "/404" && <Footer />}
     </>
   );
 }
